fe7ef9cb3237371ffa747e44687c5f90ca0f9e20
Adham Essam <adhamessamet@proton.me>
2025-12-07 15:55:00 +0200

    Change indentation to 4 spaces and for snippets

--- lua/custom/snippets/cpp-graph.lua
+++ lua/custom/snippets/cpp-graph.lua
@@ -4,18 +4,18 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("cpp", {
-      -- grid directions
-      s("grid4Dir", {
-            t{
-                  "const int dr[] = {-1, 0, 1, 0};",
-                  "const int dc[] = {0, -1, 0, 1};"
-            }
-      }),
-      s("grid8Dir", {
-            t{
-                  "const int dr[] = {-1, 0, 1, -1, 1, -1, 0, 1};",
-                  "const int dc[] = {-1, -1, -1, 0, 0, 1, 1, 1};"
-            }
-      }),
+    -- grid directions
+    s("grid4Dir", {
+        t{
+            "const int dr[] = {-1, 0, 1, 0};",
+            "const int dc[] = {0, -1, 0, 1};"
+        }
+    }),
+    s("grid8Dir", {
+        t{
+            "const int dr[] = {-1, 0, 1, -1, 1, -1, 0, 1};",
+            "const int dc[] = {-1, -1, -1, 0, 0, 1, 1, 1};"
+        }
+    }),
 })
 
